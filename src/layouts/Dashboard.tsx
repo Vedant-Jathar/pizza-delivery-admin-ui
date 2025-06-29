2fe7e38810595eede977c97709ea74a27a507ab8
Vedant_Jathar <jatharvedant16@gmail.com>
2025-06-29 22:38:16 +0530

    check user in store for redirection

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,9 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Link, Navigate, Outlet } from "react-router-dom"
+import { useAuthStore } from "../store"
 
 const Dashboard = () => {
+    const { user } = useAuthStore()
+    if (!user) {
+        return <Navigate to={"/auth/login"} />
+    }
     return (
         <>
             <div>Dashboard</div>
+            <Link to={"/auth/login"}>Go to login page</Link>
             <Outlet />
         </>
     )
