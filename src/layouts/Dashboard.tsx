d186f81ec5737500117bdf268eeef1239a854a2e
Vedant_Jathar <jatharvedant16@gmail.com>
2025-06-30 23:34:37 +0530

    add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,16 +1,78 @@
-import { Link, Navigate, Outlet } from "react-router-dom"
+import { Navigate, NavLink, Outlet, useLocation } from "react-router-dom"
 import { useAuthStore } from "../store"
+import { Layout, Menu, theme, } from "antd"
+import Sider from "antd/es/layout/Sider"
+import { Content, Footer, Header } from "antd/es/layout/layout"
+import { useState } from "react"
+import Home from "../components/icons/Home"
+import Icon from "@ant-design/icons"
+import UserIcon from "../components/icons/UserIcon"
+import Logo from "../components/icons/Logo"
 
 const Dashboard = () => {
+
+    const location = useLocation()
+    const pathName = location.pathname
+    console.log(pathName);
     
+    const [collapsed, setCollapsed] = useState(false)
+
     const { user } = useAuthStore()
     if (!user) {
         return <Navigate to={"/auth/login"} />
     }
+
+    const items = [
+        {
+            key: '/',
+            icon: <Icon component={Home} />,
+            label: <NavLink to="/">Home</NavLink>
+        },
+        {
+            key: '/users',
+            icon: <Icon component={UserIcon} />,
+            label: <NavLink to="/users">Users</NavLink>
+        },
+        {
+            key: '/restaurants',
+            icon: <Icon component={Home} />,
+            label: <NavLink to="/restaurants">Restaurants</NavLink>
+        },
+        {
+            key: '/products',
+            icon: <Icon component={Home} />,
+            label: <NavLink to="/">Products</NavLink>
+        },
+        {
+            key: '/promos',
+            icon: <Icon component={Home} />,
+            label: <NavLink to="/">Promos</NavLink>
+        },
+    ]
+
+    const {
+        token: { colorBgContainer }
+    } = theme.useToken()
+
     return (
         <>
-            <div>Dashboard</div>
-            <Link to={"/auth/login"}>Go to login page</Link>
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sider theme="light" collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                    <div className="demo-logo-vertical" id="logo_on_dashboard">
+                        <Logo />
+                    </div>
+                    <Menu theme="light" defaultSelectedKeys={[pathName]} mode="inline" items={items} />
+                </Sider>
+                <Layout>
+                    <Header style={{ padding: 0, background: colorBgContainer }} />
+                    <Content style={{ margin: '0 16px' }}>
+                        <Outlet />
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Mern-space pizza shop
+                    </Footer>
+                </Layout>
+            </Layout >
             <Outlet />
         </>
     )
